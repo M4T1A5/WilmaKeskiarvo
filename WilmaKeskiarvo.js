f14091525cf86e3910a1dd696e14865c849ac9f8
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2012-04-13 16:02:11 +0300

    Actually calculate the average

--- WilmaKeskiarvo.js
+++ WilmaKeskiarvo.js
@@ -3,11 +3,13 @@ function isNumber (o) {
 }
 
 $(document).ready(function(){
-	var keskiarvo = 0;
+	var keskiarvo = 0, arvosanoja = 0;
 	$(".undefined:nth-child(2)").each(function(){
 		if(isNumber($(this).text())){
 			keskiarvo += parseInt($(this).text(), 10);
+			arvosanoja++;
 		}
 	});
+	keskiarvo = keskiarvo / arvosanoja;
 	console.log(keskiarvo);
 });
